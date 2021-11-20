38246e6d743891321000bf32f253a5361629259a
Julien Barrier <julien.barrier@espci.org>
2021-11-20 22:44:37 +0100

    download page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+//import Home from './home';
 import Download from './download';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/download' exact component={() => <Download />} />
-      </Routes>
-    </Router>
+    <Download />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
