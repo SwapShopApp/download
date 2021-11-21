1689528a72fda9038cb19bbe998ebdccdc92975f
Julien Barrier <julien.barrier@espci.org>
2021-11-21 17:08:54 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-//import Home from './home';
 import Download from './download';
 import reportWebVitals from './reportWebVitals';
 
